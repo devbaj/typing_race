f5a733af979746e6d0fb507bb41305d54630d0c6
Brett A. Jory <brett.austin.jory@gmail.com>
2019-07-15 19:31:57 -0700

    implemented api calls and async functions for book excerpts, would like to find a better API

--- public/js/game.js
+++ public/js/game.js
@@ -5,14 +5,23 @@ $(document).ready( () => {
   $("#opponent").prop("disabled", true);
   $("#opponent").val("");
   $("#userbox").val("");
-  var template = chooseText();
-  $("#sampleText").html(template);
+  var text;
+  chooseText()
+    .then( template => {
+      console.log(template);
+      text = template;
+      $("#sampleText").html(template);
+    })
+    .catch( err => {
+      console.log("error: " + err);
+    });
+
 
   // Listen for user input
   $("#userbox").keyup( () => {
     var userInput = $("#userbox").val();
-    var status = userInputIsAccurate(template, userInput);
-    if (userInput.length == template.length && status) {
+    var status = userInputIsAccurate(text, userInput);
+    if (userInput.length == text.length && status) {
       console.log("done");
       endUserInput();
     }
@@ -32,15 +41,31 @@ socket.on("opponentFinish", () => {
 // ** game logic goes here
 // todo: eventually we will have semirandom blocks of text (API for this?)
 // todo: evaluate accuracy SO FAR
-function chooseText() {
-  // ! placeholder for now
-  var sampleText = "Lorem ipsum and all the rest etc etc";
-  return sampleText;
+async function chooseText() {
+  var ISBN = "ISBN:0007322607";
+  var template = await getBookInfo(ISBN);
+  console.log("template info from inside async function" +template);
+  return template;
+
+}
+
+function getBookInfo(isbn) {
+  return new Promise( resolve => {
+  $.ajax("https://openlibrary.org/api/books?bibkeys=" + isbn + "&jscmd=data&format=json")
+    .done( data => {
+      console.log(data);
+      var sampleText = data[isbn].excerpts[0].text;
+      console.log(sampleText);
+      resolve(sampleText);
+    });
+  });
 }
 
 function userInputIsAccurate(text, userText) {
   var progress = userText.length;
   var slicedTemplate = text.slice(0, progress);
+  console.log("user text: " + userText );
+  console.log("sample text: " + slicedTemplate);
   if (userText == slicedTemplate) {
     $("#status").html("good");
     return true;
